0f076faae2832597501b5ab06d413dfa0dc2ad42
Gosia Antoniak/gosia-antoniak_89@o2.pl <gosia-antoniak_89@o2.pl>
2022-09-02 22:18:30 +0200

    Initial commit - customer reviews

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+
+// Dodany i uruchomiony skrypt do Splide (karuzela)
+import Splide from '@splidejs/splide';
+
+new Splide('.splide', {
+  arrows: false,
+  rewind: true,
+  type: 'slide',
+  speed: 500,
+  autoplay: true,
+  interval: 500,
+  pauseOnHover: true,
+  perPage: 1,
+  preloadPages: 1,
+}).mount();
+
